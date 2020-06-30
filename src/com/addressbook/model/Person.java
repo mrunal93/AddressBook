0c31fe7a39eba1fe9db5fe13d08f6f80050f048d
mrn26893@gmail.com <mrn26893@gmail.com>
2020-06-30 09:47:39 +0530

    update file

--- src/com/addressbook/model/Person.java
+++ src/com/addressbook/model/Person.java
@@ -1,81 +1,88 @@
 package com.addressbook.model;
-import com.addressbook.model.Address;
-import java.lang.Comparable;
-import java.io.Serializable;
+import java.util.*;
+import java.io.*;
+public class Person implements Comparable<Person>,Serializable{
+	static String address;
+	static String name;
+	static String email;
+	static String mobile;
+	static String state;
+	static String city;
+	static double zip;
+	Person(){}
+	Person(String address, String name, String city , String state, String email, String mobile , double zip){
+		this.address=address;
+		this.name=name;
+		this.email=email;
+		this.mobile=mobile;
+		this.state=state;
+		this.city=city;
+		this.zip=zip;
+	}
 
-public class Person implements Comparable<Person>,Serializable
-{
-        private static final long serialVersionUID = 1L;
-        String firstName;
-        String lastName;
-        Address address;
-        String phoneNumber;
-                Person(){}
-                Person(String firstName, String lastName,Address address, String phoneNumber)
-                {
-                        this.firstName = firstName;
-                        this.lastName = lastName;
-                        this.address = address;
-                        this.phoneNumber = phoneNumber;
-                }
+	public void setAddress(){
+		this.address = address;
+	}
 
-                public String getFirstName()
-                {
-                        return this.firstName;
-                }
+	public void setName(String name){
+		this.name=name;
+	}
+	public void setCity(String city){
+		this.city=city;
+	}
+	public void setState(String state){
+		this.state=state;
+	}
 
-                public void setFirstName( String firstName)
-                {
-                         this.firstName = firstName;
-                }
+	public void setemail(String email){
+                this.email=email;
+        }
+	public void setmobile(String mobile){
+                this.mobile=mobile;
+        }
 
-                public String getLastName()
-                {
-                        return this.lastName;
-                }
-
-                public void setLastName( String lastName)
-               {
-                         this.lastName = lastName;
-               }
-
-                public Address getAddress()
-                {
-                        return this.address;
-                }
+	public void setzip(double zip){
+		this.zip=zip;
+	}
 
 
-                public void setAddress(Address address)
-                {
-                        this.address = address;
-                }
+	public String getAddress(){
+		return this.address;
+	}
 
-                public String getPhoneNumber()
-                {
-                        return this.phoneNumber;
-                }
-		public void setPhoneNumber(String phoneNumber)
-                {
-                        this.phoneNumber = phoneNumber;
+	public String getName(){
+		return this.name;
+	}
 
-                }
+	public String getEmail(){
+		return this.email;
+	}
+	public String getCity(){
+		return this.city;
+	}
 
-                public String toString()
-                {
-                        return this.firstName +" "+ this.lastName +"\n"+ this.phoneNumber +"\n"+ this.address.toString();
-                }
+	public String getState(){
+		return this.state;
+	}
+	public String getmobile(){
+		return this.mobile;
+	}
+	public double getzip(){
+		return this.zip;
+	}
+	
+	public String toString(){
+		return this.address +"\n" +this.city + "\n" +this.name +"\n" +this.email + "\n" +this.zip;
+	}
 
-                public int compareTo(Person person)
-                {
-                        if(this.address.getZip() > person.address.getZip())
-                        {
-                                return 1;
-                        }
-                        if(this.address.getZip() < person.address.getZip())
-                        {
-                                return -1;
-                        }
-                        return 0;
+	public int compareTo(Person person){
+		if(this.address.getzip() > person.address.getzip()){
+			return 1;
+		}
+		if(this.address.getzip() < person.address.getzip()){
+			return -1;
+		}
+		return 0;
+	}
 
-                }
 }
